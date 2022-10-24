d0241d01334974ff55c5ecc25b1f79172f1ae221
shivaji <shivavishnoi.vishnoi@gmail.com>
2022-10-24 09:35:22 +0530

    Validations

---
+++ block-BNaacj/Code/models/schema.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema(
+  {
+    password: { type: String, minlength: 5, maxlength: 15 },
+    createdAt: { type: Date, default: new Date() },
+  },
+  { timestamps: true }
+);
