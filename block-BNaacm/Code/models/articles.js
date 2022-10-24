5f1b08c821d75946ebd12b81be4a6cef33ee9ada
shivaji <shivavishnoi.vishnoi@gmail.com>
2022-10-24 10:59:29 +0530

    Models

---
+++ block-BNaacm/Code/models/articles.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var articleSchema = new Schema({
+  title: { type: String },
+  Pages: { type: Number, min: 1, max: 5 },
+});
+
+.0.0
+0
+0.
+
+var Article = mongoose.model('Article', articleSchema);
+module.exports = Article;
